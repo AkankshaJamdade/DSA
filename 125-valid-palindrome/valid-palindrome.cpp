f26c02579655d0dd8bd21b792b239e402f70f51e
AkankshaJamdade <akankshajamdade2007@gmail.com>
2026-05-02 22:48:06 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (52.82%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -5,18 +5,22 @@ public:
         int right=s.length()-1;
 
         while(left<right){
-            while(left<right && !isalnum(s[left]))left++;
-            while(left<right && !isalnum(s[right]))right--;
-
+            while(left<right && !isalnum(s[left])) left++;
+            while(left<right && !isalnum(s[right])) right--;
 
             if(tolower(s[left]) != tolower(s[right])){
                 return false;
             }
 
             left++;
-            right--;    
+            right--;
+
         }
 
+
         return true;
-    }
+
+
+
+   }
 };
