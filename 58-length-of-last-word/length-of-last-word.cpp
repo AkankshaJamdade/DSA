ade95a7fc37bdda5402b561209687cf348f4791a
AkankshaJamdade <akankshajamdade2007@gmail.com>
2026-05-12 23:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (8.26%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        int length = 0;
+
+        // skip trailing spaces
+        while(i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        // count last word
+        while(i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
